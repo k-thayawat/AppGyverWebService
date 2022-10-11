66b6e5147867cb9b748e8c6c679d15e11e2ad022
Thayawat Thathong <tthayawat@netizen.co.th>
2022-10-11 17:53:00 +0700

    test Ws

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log( data );
       res.end( data );
+      res.status;
    });
 })
 
